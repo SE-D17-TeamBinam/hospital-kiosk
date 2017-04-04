60f4764dea9ad93f000c698958c246763a620f63
Alberto Ramirez <aramirez2@wpi.edu>
2017-04-04 19:53:39 -0400

    Update Node

--- src/Point.java
+++ src/Point.java
@@ -4,26 +4,42 @@ import java.util.ArrayList;
  * Created by Alberto on 3/30/2017.
  */
 public class Point {
-    int xCoord;    //X coordinate
-    int yCoord;    //Y coordinate
-    String name;
-    String id;      //Name of the Point
-    private static ArrayList<Point> neighbors;    //HashMap of all of the nieghbors, assigned by name
-    Point parent;
-    int cost;
+  int xCoord;    //X coordinate
+  int yCoord;    //Y coordinate
+  String name;  //Name of the room
+  String id;      //Unique Identifier
+  int floor;
+  public static ArrayList<Point> neighbors;
+  //Attributes For A* only below.
+  Point parent;
+  int cost;
 
-    //Constructor
-    public Point(int xCoord, int yCoord, String id,ArrayList<Point> neighbors){
-      this.xCoord = xCoord;
-      this.yCoord = yCoord;
-      this.id = id;
-      this.neighbors = neighbors;
-      this.parent = null;
-      this.cost = 0;
-    }
+  //Constructor
+  public Point(int xCoord, int yCoord, String name, String id, ArrayList <Point> new_neighbors, int floor){
+    this.xCoord = xCoord;
+    this.yCoord = yCoord;
+    this.name = name;
+    this.id = id;
+    this.parent = null;
+    this.neighbors = new_neighbors;
+    this.cost = 0;
+    this.floor = floor;
+  }
 
-    //Methods
-    public void addParent(Point padre){
-        this.parent = padre;
-    }
+  //Methods
+  public void addParent(Point padre){
+    this.parent = padre;
+  }
+
+  public int Heuristic(Point End){
+    int x = Math.abs(this.xCoord - End.xCoord);
+    int y = Math.abs(this.yCoord - End.yCoord);
+    return x+y;
+  }
+
+  public int Distance(Point End){//Straight Line Distance
+    double x = End.xCoord - this.xCoord;
+    double y = End.yCoord - this.yCoord;
+    return (int) Math.sqrt(x*x + y*y);
+  }
 }

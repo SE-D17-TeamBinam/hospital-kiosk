4ca894416860d7d8cec5596788e98d92845ca352
Evan Duffy <eduffy2015@spyponders.com>
2017-04-03 23:48:44 -0400

    Added Database Editor Skeleton To Be Populated Tomorrow

---
+++ src/DatabaseEditor.java
@@ -0,0 +1,63 @@
+/**
+ * Database Editor
+ * Abstracts the SQL Away from the rest of the program by providing methods to add / remove / edit elements of the database directly in java methods
+ * Created by Evan on 4/3/2017.
+ */
+public class DatabaseEditor {
+  DatabaseController dbc;
+
+  DatabaseEditor(DatabaseController _dbc){
+    this.dbc = _dbc;
+  }
+
+
+  ///////////////////////////
+  ///////// Service /////////
+  ///////////////////////////
+
+
+
+
+
+
+
+
+  ///////////////////////////
+  //////// Provider /////////
+  ///////////////////////////
+
+
+
+
+
+
+  ///////////////////////////
+  //////// Location /////////
+  ///////////////////////////
+
+
+
+
+
+
+  ///////////////////////////
+  //// Location - Service ///
+  ///////////////////////////
+
+
+
+
+
+
+
+  ///////////////////////////
+  /// Location - Provider ///
+  ///////////////////////////
+
+
+
+
+
+
+
+}
